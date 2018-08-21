e684c43ddb674856019bc450b2ba794834493625
Coordenação <Coordenao@Coordenao-PC>
2018-08-21 19:51:48 -0300

    Commit de teste na escola

---
+++ application/views/projeto_escola.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Teste escola</title>
+<body>
+    <h1>Primeiro teste da escola!</h1>
+</body>
