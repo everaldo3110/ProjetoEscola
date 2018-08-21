a3ff2c2c1951fd176dddf466bb854a483910f216
Coordenação <Coordenao@Coordenao-PC>
2018-08-21 20:00:46 -0300

    Commit de teste na escola

--- application/views/projeto_escola.php
+++ application/views/projeto_escola.php
@@ -1,10 +1,14 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
 	<meta charset="utf-8">
 	<title>Teste escola</title>
 <body>
     <h1>Primeiro teste da escola!</h1>
+    <?php
+    //alteração no Netbeans!
+    ?>
+    
 </body>
